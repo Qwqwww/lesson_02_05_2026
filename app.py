ad460c383671a8d42b517d68d969c8edf390693c
Qwqwww <jollot@yandex.ru>
2026-05-02 19:10:05 +0700

    Добавлена форма для обработки

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["post", "get"])
 def index():
-    return "Hello!"
+    message = ""
+    if request.method == "POST":
+        area = request.form.get("area")
+        message = int(area) * 300_000
+    return render_template("index.html", message=message)
 
 if __name__ == "__main__":
     app.run()
